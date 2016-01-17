b09c0a81683c0f09706f89e085b5738b2f2c66f4
Martin Cigorraga <martincigorraga@gmail.com>
2016-01-17 01:56:44 -0300

    Blah

--- bb.sh
+++ bb.sh
@@ -498,6 +498,7 @@ create_html_page() {
         [[ $index == no ]] && disqus_body
 
         # page footer
+        echo '</br>'
         google_search  # i90rr.mark
         cat .footer.html
         # close divs
